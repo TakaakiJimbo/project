40423fa757312f1139aa581341bc274e75052deb
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-12-31 09:30:51 +0900

    enable to show start visual

---
+++ Assets/Scripts/MainButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainButton : MonoBehaviour {
+
+	GameObject startButton;
+	GameObject howToPlayButton;
+	GameObject intro;
+
+	void Start() {
+		startButton     = transform.FindChild ("START").gameObject;
+		howToPlayButton = transform.FindChild ("HowToPlay").gameObject;
+		intro           = transform.FindChild ("Intro").gameObject;
+		intro.SetActive (false);
+	}
+
+	public void clickStartButton () {
+		Application.LoadLevel("debug");
+	}
+
+	public void clickHowToPlayButton () {
+		intro.SetActive (true);
+	}
+
+	public void clickIntro () {
+		intro.SetActive (false);
+	}
+}
