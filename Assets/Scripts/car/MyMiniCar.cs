9ce1c41de9cfbaf44fbde821aa22852db0741058
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2015-12-31 11:50:40 +0900

    enable to move mini cars

---
+++ Assets/Scripts/car/MyMiniCar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMiniCar : MonoBehaviour {
+
+	private Vector3 positionInScreen;
+	private Vector3 startPosition;
+	private Camera camera;
+	private double margin = 1.5;
+
+	void OnEnable () {
+		camera = Camera.main;
+		startPosition = gameObject.transform.position;
+		gameObject.GetComponent<Rigidbody>().velocity = new Vector3 (Random.Range(8, 13), 0, 0);
+	}
+
+	void Update () {
+		if (isOutOfScreen()) {
+			gameObject.SetActive (false);
+		}
+	}
+
+	bool isOutOfScreen() {
+		positionInScreen = camera.WorldToViewportPoint(transform.position);
+		if (positionInScreen.x >= margin) {
+			gameObject.transform.position = startPosition;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
---
+++ Assets/Scripts/car/MyMiniCarController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyMiniCarController : MonoBehaviour {
+
+	private int count ;
+
+	void Start () {
+		count = gameObject.transform.childCount;
+		StartCoroutine(loop());
+	}
+	
+	private IEnumerator loop() {
+		while (true) {
+			yield return new WaitForSeconds(1f);
+			enableMiniCar();
+		}
+	}
+
+	private void enableMiniCar() {
+		gameObject.transform.GetChild (Random.Range (0, count)).gameObject.SetActive (true);
+	}
+}
