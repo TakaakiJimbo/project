be50d9ed2d4843b915f67a6277024839aff54b74
AkihiroKOBAYASHI <sun.moon.bow.reverse7@gmail.com>
2016-01-30 13:42:04 +0900

    enable to effectivity up

--- Assets/Scripts/game/car/MyCarLifePoint.cs
+++ Assets/Scripts/game/car/MyCarLifePoint.cs
@@ -13,7 +13,6 @@ public class MyCarLifePoint : MyCar {
 
 	void Start() {
 		reflectLifePoint();
-		Debug.Log(PlayerPrefs.GetInt("Result1", 0));
 		targetcamera.showHighScore(PlayerPrefs.GetInt("Result1", 0));
 		for(int i = 0;i < 10;i++) {
 			scoreArray[i] = PlayerPrefs.GetInt("Result" + (i + 1), 0);
@@ -88,7 +87,6 @@ public class MyCarLifePoint : MyCar {
 		}
 		for(int i = 0;i < 10;i++) {
 			PlayerPrefs.SetInt("Result" + (i + 1), scoreArray[i]);
-			Debug.Log("scoreArray[" + i + "]:" + scoreArray[i]);
 		}
 		targetcamera.showHighScore(PlayerPrefs.GetInt("Result1"));
 	}
